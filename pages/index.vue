c2bd51ceb2a121241b29963f80804b2f7489e475
Mir Imad Ahmed <mirimadahmed@outlook.com>
2019-11-28 12:31:10 +0500

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <CustomSwitch v-model="switchVal" />
+    <CustomSwitch v-model="switchVal1" />
+    <CustomSwitch v-model="switchVal2" />
+  </div>
+</template>
+
+<script>
+import CustomSwitch from '~/components/CustomSwitch.vue'
+
+export default {
+  components: {
+    CustomSwitch
+  },
+  data () {
+    return {
+      switchVal: 1,
+      switchVal1: 2,
+      switchVal2: 3
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 50 auto;
+  min-height: 100vh;
+}
+</style>
