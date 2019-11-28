1eb6400b9c09b209a595fdd44475e5bd8e2a2d93
Mir Imad Ahmed <mirimadahmed@outlook.com>
2019-11-28 12:40:37 +0500

    basic nav added

---
+++ components/Nav.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-navbar class="bg">
+    <b-navbar-brand href="#">
+      LOGO
+    </b-navbar-brand>
+    <b-navbar-nav class="ml-auto">
+      <em>POI</em>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.bg {
+  background: #013A71;
+  border-bottom: 10px #CD0D2B solid;
+}
+</style>
