662e90bfdbccda18aafb7d1fe7d27ce926e7fdbd
Mir Imad Ahmed <mirimadahmed@outlook.com>
2019-11-28 13:40:38 +0500

    new page scene

---
+++ pages/_asset/_vehicle.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <Header />
+    <Login />
+    <CustomSwitch v-model="switchVal" />
+    <CustomSwitch v-model="switchVal1" />
+    <CustomSwitch v-model="switchVal2" />
+  </div>
+</template>
+
+<script>
+import CustomSwitch from '~/components/CustomSwitch.vue'
+import Login from '~/components/Login.vue'
+import Header from '~/components/Header.vue'
+
+export default {
+  components: {
+    CustomSwitch,
+    Header,
+    Login
+  },
+  data () {
+    return {
+      switchVal: 1,
+      switchVal1: 2,
+      switchVal2: 3
+    }
+  }
+}
+</script>
+
+<style>
+</style>
