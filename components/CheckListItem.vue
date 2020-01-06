c4d0eeb8f03d81ed6ec2566f819fb5660ba59dfd
Mir Imad Ahmed <mirimadahmed@outlook.com>
2020-01-06 07:06:20 +0500

    used new toggle

--- components/CheckListItem.vue
+++ components/CheckListItem.vue
@@ -1,17 +1,24 @@
 <template>
   <div class="my-2">
-    <div :class="local_value.value === 2 ? 'red-bg' : 'white-bg'" class="row m-0 p-2 shadow-sm">
+    <div :class="!local_value.value ? 'red-bg' : 'white-bg'" class="row m-0 p-2 shadow-sm">
       <div class="col py-2">
         {{ local_value.title }}
       </div>
       <div class="col py-2 text-center">
         <a :href="local_value.video" class="stretched-link">&#9432;</a>
       </div>
-      <div class="col p-0">
-        <custom-switch v-model="local_value.value" />
+      <div class="col px-0 py-1">
+        <toggle-button
+          v-model="local_value.value"
+          :height="30"
+          :width="70"
+          :labels="labels"
+          :color="colors"
+          class="m-0 float-right"
+        />
       </div>
     </div>
-    <div v-if="local_value.value === 2" class="row m-0 p-2 red-light-bg">
+    <div v-if="!local_value.value" class="row m-0 p-2 red-light-bg">
       <div class="col-md-12 m-0 my-2 p-0">
         <b-form-textarea
           id="textarea"
@@ -37,11 +44,7 @@
 </template>
 
 <script>
-import CustomSwitch from '~/components/CustomSwitch.vue'
 export default {
-  components: {
-    CustomSwitch
-  },
   props: {
     value: {
       type: Object,
@@ -50,7 +53,9 @@ export default {
   },
   data () {
     return {
-      local_value: this.value
+      local_value: this.value,
+      labels: { checked: 'PASS', unchecked: 'FAIL' },
+      colors: { checked: '#C0D107', unchecked: '#E72302' }
     }
   },
   watch: {
@@ -68,7 +73,7 @@ export default {
   background: white;
 }
 .red-bg {
-  background: #E31836;
+  background: rgb(221, 88, 108);
 }
 .red-light-bg {
   background: #E7CDCE;
